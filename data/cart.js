cb7bf4f81422a5c355b3a9ec256d4995a9534da0
Mazen Essam <Mazenessam2011@outlook.com>
2024-07-09 21:36:44 +0300

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
-export const cart=[{
+export const cart=JSON.parse(localStorage.getItem('cart'))||[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
   },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
-
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 export function addToCart(productId){
   let selectedItem;
   cart.forEach((item)=>{
@@ -25,6 +27,7 @@ export function addToCart(productId){
       quantity
     });
   }
+  saveToStorage();
 }
 export function removeFromCart(productId){
   cart.forEach((cartItem)=>{
@@ -35,4 +38,5 @@ export function removeFromCart(productId){
      
     }
   });
+  saveToStorage();
 }
